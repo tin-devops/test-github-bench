04ce2903393f06621db5cd88a5522ca5724f2135
github-action-benchmark <github@users.noreply.github.com>
2022-10-30 11:57:53 +0000

    add Rust Benchmark (cargo) benchmark result for da6bc9f16cef0ddab5720cb5cb6720f16c2ddb57

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1667131073917,
+  "repoUrl": "https://github.com/tin-devops/test-github-bench",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "tindevops@gmail.com",
+            "name": "tindevops",
+            "username": "DevOpsSenior"
+          },
+          "committer": {
+            "email": "tindevops@gmail.com",
+            "name": "tindevops",
+            "username": "DevOpsSenior"
+          },
+          "distinct": true,
+          "id": "da6bc9f16cef0ddab5720cb5cb6720f16c2ddb57",
+          "message": "Add test",
+          "timestamp": "2022-10-30T18:57:14+07:00",
+          "tree_id": "3c6e4cf7b64ff8d4628fea5b1f921e7ad49f736a",
+          "url": "https://github.com/tin-devops/test-github-bench/commit/da6bc9f16cef0ddab5720cb5cb6720f16c2ddb57"
+        },
+        "date": 1667131073303,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "bench_fib_10",
+            "value": 222,
+            "range": "± 1",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "bench_fib_20",
+            "value": 28389,
+            "range": "± 136",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
