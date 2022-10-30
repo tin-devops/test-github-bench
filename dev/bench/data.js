c030c2c60da99eea43eefb5086cddfd69c3b31e6
github-action-benchmark <github@users.noreply.github.com>
2022-10-30 12:00:48 +0000

    add Rust Benchmark (cargo) benchmark result for da6bc9f16cef0ddab5720cb5cb6720f16c2ddb57

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1667131073917,
+  "lastUpdate": 1667131248576,
   "repoUrl": "https://github.com/tin-devops/test-github-bench",
   "entries": {
     "Rust Benchmark": [
@@ -35,6 +35,66 @@ window.BENCHMARK_DATA = {
             "name": "bench_fib_20",
             "value": 28389,
             "range": "± 136",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "tindevops@gmail.com",
+            "name": "tindevops",
+            "username": "DevOpsSenior"
+          },
+          "committer": {
+            "email": "tindevops@gmail.com",
+            "name": "tindevops",
+            "username": "DevOpsSenior"
+          },
+          "distinct": true,
+          "id": "da6bc9f16cef0ddab5720cb5cb6720f16c2ddb57",
+          "message": "Add test",
+          "timestamp": "2022-10-30T18:57:14+07:00",
+          "tree_id": "3c6e4cf7b64ff8d4628fea5b1f921e7ad49f736a",
+          "url": "https://github.com/tin-devops/test-github-bench/commit/da6bc9f16cef0ddab5720cb5cb6720f16c2ddb57"
+        },
+        "date": 1667131247968,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "BenchFib10",
+            "value": 178,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "BenchFib20",
+            "value": 23992,
+            "range": "± 35",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Fibonacci/Recursive/20",
+            "value": 23996,
+            "range": "± 31",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Fibonacci/Iterative/20",
+            "value": 5,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Fibonacci/Recursive/21",
+            "value": 38843,
+            "range": "± 58",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Fibonacci/Iterative/21",
+            "value": 6,
+            "range": "± 0",
             "unit": "ns/iter"
           }
         ]
